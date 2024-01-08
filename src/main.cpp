728a61127ef797a28b22902cc88cdd89148c81a8
Christina A. Upshaw <chrisaupshaw@gmail.com>
2024-01-07 20:35:25 -0800

    Comment out almost everything.

--- src/main.cpp
+++ src/main.cpp
@@ -89,6 +89,7 @@ const char* password = "littleoctopus244";
   #define VSYNC_GPIO_NUM    25
   #define HREF_GPIO_NUM     23
   #define PCLK_GPIO_NUM     22
+  #define LED_NUM            4
 #else
   #error "Camera model not selected"
 #endif
@@ -153,7 +154,7 @@ static esp_err_t stream_handler(httpd_req_t *req){
     if(res != ESP_OK){
       break;
     }
-    //Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
+    Serial.printf("MJPG: %uB\n",(uint32_t)(_jpg_buf_len));
   }
   return res;
 }
@@ -169,7 +170,7 @@ void startCameraServer(){
     .user_ctx  = NULL
   };
   
-  //Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+  Serial.printf("Starting web server on port: '%d'\n", config.server_port);
   if (httpd_start(&stream_httpd, &config) == ESP_OK) {
     httpd_register_uri_handler(stream_httpd, &index_uri);
   }
@@ -177,67 +178,76 @@ void startCameraServer(){
 
 void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  pinMode(LED_NUM, OUTPUT);
+  digitalWrite(LED_NUM, LOW);
  
-  Serial.begin(115200);
-  Serial.setDebugOutput(false);
-  while (!Serial);
+  // Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+  // while (!Serial);
+  digitalWrite(LED_NUM, HIGH);
   
-  camera_config_t config;
-  config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
-  config.pin_sccb_sda = SIOD_GPIO_NUM;
-  config.pin_sccb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
-  config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG; 
+  // camera_config_t config;
+  // config.ledc_channel = LEDC_CHANNEL_0;
+  // config.ledc_timer = LEDC_TIMER_0;
+  // config.pin_d0 = Y2_GPIO_NUM;
+  // config.pin_d1 = Y3_GPIO_NUM;
+  // config.pin_d2 = Y4_GPIO_NUM;
+  // config.pin_d3 = Y5_GPIO_NUM;
+  // config.pin_d4 = Y6_GPIO_NUM;
+  // config.pin_d5 = Y7_GPIO_NUM;
+  // config.pin_d6 = Y8_GPIO_NUM;
+  // config.pin_d7 = Y9_GPIO_NUM;
+  // config.pin_xclk = XCLK_GPIO_NUM;
+  // config.pin_pclk = PCLK_GPIO_NUM;
+  // config.pin_vsync = VSYNC_GPIO_NUM;
+  // config.pin_href = HREF_GPIO_NUM;
+  // config.pin_sccb_sda = SIOD_GPIO_NUM;
+  // config.pin_sccb_scl = SIOC_GPIO_NUM;
+  // config.pin_pwdn = PWDN_GPIO_NUM;
+  // config.pin_reset = RESET_GPIO_NUM;
+  // config.xclk_freq_hz = 20000000;
+  // config.pixel_format = PIXFORMAT_JPEG; 
   
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
-  } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
-  }
+  // if(psramFound()){
+  //   config.frame_size = FRAMESIZE_UXGA;
+  //   config.jpeg_quality = 10;
+  //   config.fb_count = 2;
+  // } else {
+  //   config.frame_size = FRAMESIZE_SVGA;
+  //   config.jpeg_quality = 12;
+  //   config.fb_count = 1;
+  // }
   
-  // Camera init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-    Serial.printf("Camera init failed with error 0x%x", err);
-    return;
-  }
-  // Wi-Fi connection
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
+  // // Camera init
+  // esp_err_t err = esp_camera_init(&config);
+  // if (err != ESP_OK) {
+  //   Serial.printf("Camera init failed with error 0x%x", err);
+  //   return;
+  // }
+  // // Wi-Fi connection
+  // WiFi.begin(ssid, password);
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  // }
+  // Serial.println("");
+  // Serial.println("WiFi connected");
   
-  Serial.print("Camera Stream Ready! Go to: http://");
-  Serial.print(WiFi.localIP());
+  // Serial.print("Camera Stream Ready! Go to: http://");
+  // Serial.print(WiFi.localIP());
   
-  // Start streaming web server
-  startCameraServer();
+  // // Start streaming web server
+  // startCameraServer();
 }
 
+bool light = false;
+
 void loop() {
-  delay(1000);
-  Serial.print("Camera Stream Ready! Go to: http://");
-  Serial.print(WiFi.localIP().toString());
+  // delay(500);
+  // // Serial.print("Camera Stream Ready! Go to: http://");
+  // // Serial.print(WiFi.localIP().toString());
+  // light = !light;
+  // digitalWrite(LED_NUM, light ? HIGH : LOW);
+  // // Serial.printf("Light = %s\n", light ? "true" : "false");
+  // // Serial.flush();
 }
